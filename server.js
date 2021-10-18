dab51af7e88e6e2fbe2eb45ba4c2737ed09f6649
Valerie <valerie.tram.anh.n@gmail.com>
2021-10-18 13:49:48 -0400

    handle requests for a specific animal

--- server.js
+++ server.js
@@ -44,12 +44,23 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
-app.get('/api/animals', (req, res) => {
-    let results = animals;
-    if (req.query) {
-      results = filterByQuery(req.query, results);
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
     }
-    res.json(results);
+  });
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+      res.json(result);
 });
 
 app.listen(PORT, () => {
